de7016991fe607c4e0e9aecf6930708cba87dd18
888dandan <1550713714@qq.com>
2018-03-08 17:43:20 +0800

    第二次

--- xib创建tableViewCell/ViewController.m
+++ xib创建tableViewCell/ViewController.m
@@ -7,23 +7,77 @@
 //
 
 #import "ViewController.h"
+#define   rgb(a,b,c)    [UIColor colorWithRed:(a)/255.0 green:(b)/255.0 blue:(c)/255.0 alpha:(1)]
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UITableView *myTableView;
+@property (strong, nonatomic) IBOutlet UITableViewCell *cell0;
+@property (strong, nonatomic) IBOutlet UITableViewCell *cell1;
+@property (strong, nonatomic) IBOutlet UITableViewCell *cell2;
+@property (strong, nonatomic) IBOutlet UITableViewCell *cell3;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor=[UIColor whiteColor];
+    self.myTableView.backgroundColor=rgb(255, 255, 255);
+    self.myTableView.separatorStyle=UITableViewCellSeparatorStyleNone;
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 40;
+}
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+    return 4;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    switch (indexPath.row) {
+        case 0:
+            return _cell0;
+            break;
+        case 1:
+            return _cell1;
+            break;
+        case 2:
+            return _cell2;
+            break;
+        case 3:
+            return _cell3;
+            break;
+        default:
+            break;
+    }
+    return nil;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+}
+
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
